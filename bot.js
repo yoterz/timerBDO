c49b86d9f46333a0e4981fe6df56e7c3e40d859c
yoterz <40376255+yoterz@users.noreply.github.com>
2018-06-24 13:43:45 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+bot.login(process.env.BOT_TOKEN)
+
+bot.on("ready",() => {
+    console.log('Ready...')
+    chktime()
+    
+})
+
+
+bot.on("message", (msg) => {
+    if (msg.content === "aaa"){
+        
+        msg.channel.send(thTimeZone)
+        msg.channel.send(h+' '+m)
+       
+    }
+
+  })
+  
+function timestart(){
+var currentUtcTime = new Date(); // This is in UTC
+var thTimeZone = new Date(currentUtcTime.toLocaleString('en-US', { timeZone: 'Asia/Bangkok' }));
+
+day= thTimeZone.getDay()   
+d = thTimeZone.getDate();
+month = thTimeZone.getMonth();
+year = thTimeZone.getFullYear();
+h = thTimeZone.getHours()
+m = thTimeZone.getMinutes()   
+s = thTimeZone.getSeconds();
+days = new Array('Sun', 'Monร์', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');    
+months = new Array('Jan', 'feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+
+}
